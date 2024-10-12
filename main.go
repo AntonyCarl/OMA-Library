56eb57057c0578f979a0c5ec55a7fdd0b4cc49a5
AntonyCarl <carldevgo@gmail.com>
2024-10-12 23:33:59 +0300

    Add config to Storage Refact Config method SetConfig

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"net/http"
 
-	"github.com/AntonyCarl/OMA-Library/internal/storage/postgres"
+	"github.com/AntonyCarl/OMA-Library/internal/config"
+	"github.com/AntonyCarl/OMA-Library/internal/handlers"
+	"github.com/AntonyCarl/OMA-Library/internal/storage"
 	"github.com/AntonyCarl/OMA-Library/pkg/logger"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	cfg := config.SetConfig()
 	logger.Init()
-	storage, _ := postgres.NewStorage()
-	logger.Logger.Info("start")
-	handler.RunWeb(storage)
+	storage, _ := storage.NewStorage(cfg)
+	handlers.RunWeb(storage)
 	http.ListenAndServe(":8080", nil)
 }
